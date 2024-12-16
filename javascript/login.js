2da68ef9d828a3c233db79158a348c64da6e4829
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-16 17:09:17 +0200

    added user.php

--- javascript/login.js
+++ javascript/login.js
@@ -1,20 +1,17 @@
 const form = document.querySelector(".login form"),
-  continueBtn = form.querySelector(".button input"),
   errorText = form.querySelector(".error-txt");
 
 form.onsubmit = (e) => {
   e.preventDefault();
-};
 
-continueBtn.onclick = () => {
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "php/login.php", true);
   xhr.onload = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         let data = xhr.response;
-        if (data == "success") {
-          location.href = "users.php";
+        if (data === "success") {
+          location.href = "users.php"; 
         } else {
           errorText.textContent = data;
           errorText.style.display = "block";
@@ -22,6 +19,7 @@ continueBtn.onclick = () => {
       }
     }
   };
+
   let formData = new FormData(form);
   xhr.send(formData);
 };
