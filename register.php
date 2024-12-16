269a3c665ccd0368c56d38aa83888fb0105e0d52
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-16 16:15:46 +0200

    added header file in php

--- register.php
+++ register.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>ChatSphere</title>
-  <link rel="stylesheet" href="stylee.css" />
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
-
-</head>
 <?php
 include_once("php/header.php");
 ?>
