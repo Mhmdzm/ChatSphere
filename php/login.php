86b6c4684de9da956e9258ce97696f4a0f2f24cb
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-17 10:32:33 +0200

    fixed the status in login.php

--- php/login.php
+++ php/login.php
@@ -1,29 +1,25 @@
-<?php
-include_once("config.php");
+<?php 
+    session_start();
+    include_once("config.php");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = trim(mysqli_real_escape_string($conn, $_POST['email']));
-    $password = trim(mysqli_real_escape_string($conn, $_POST['password']));
+    $email = mysqli_real_escape_string($conn,$_POST["email"]);
+    $password = mysqli_real_escape_string($conn,$_POST["password"]);
 
 
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-
-    if (mysqli_num_rows($sql) > 0) {
-        $row = mysqli_fetch_assoc($sql);
-
-        if ($password === $row['password']) {
-
-            session_start();
-            $_SESSION['unique_id'] = $row['unique_id'];
-            echo "success";
-        } else {
-
-            echo "Input Password: '" . $password . "'<br>";
-            echo "Database Password: '" . $row['password'] . "'<br>";
-            echo "Incorrect password!";
+    if(!empty($email)&&!empty($password)){
+        $sql = mysqli_query($conn,"SELECT * FROM users where email ='{$email}' AND password = '{$password}'");
+        if(mysqli_num_rows($sql)>0){
+            $row = mysqli_fetch_assoc($sql);
+            $status = "Active now";
+            $sql2 = mysqli_query($conn,"UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
+            if($sql2){
+                $_SESSION["unique_id"]=$row["unique_id"];
+                echo "success";
+            }
+        }else{
+            echo "email or password is incorrect!";
         }
-    } else {
-        echo "This email is not registered!";
+    }else{
+        echo "username and password required!";
     }
-}
 ?>
